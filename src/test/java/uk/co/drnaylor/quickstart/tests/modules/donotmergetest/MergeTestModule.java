2e0d7f990ff5ad8447d5aae144a05dbdc18fc410
Daniel Naylor <git@drnaylor.co.uk>
2017-03-30 20:52:45 +0100

    Add missing licence headers.

--- src/test/java/uk/co/drnaylor/quickstart/tests/modules/donotmergetest/MergeTestModule.java
+++ src/test/java/uk/co/drnaylor/quickstart/tests/modules/donotmergetest/MergeTestModule.java
@@ -1,3 +1,7 @@
+/*
+ * This file is part of QuickStart Module Loader, licensed under the MIT License (MIT). See the LICENSE.txt file
+ * at the root of this project for more details.
+ */
 package uk.co.drnaylor.quickstart.tests.modules.donotmergetest;
 
 import uk.co.drnaylor.quickstart.Module;
